e14de773bb0b73b65bf478aff5378c9aa32e4759
Karen <karen.avakov2@gmail.com>
2025-06-09 22:40:45 +0300

    обновил корс

--- middlewares/corsMiddleware.js
+++ middlewares/corsMiddleware.js
@@ -1,7 +1,11 @@
 import cors from 'cors';
 
 const corsOptions = {
-  origin: ['http://localhost:5173', 'http://localhost:4173'],
+  origin: [
+    'http://localhost:5173',
+    'http://localhost:4173',
+    'https://taskflow-blyt.onrender.com',
+  ],
   methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
   credentials: true,
 };
