a33691b604c22a0bff7ad000375ff33b9528f5ae
Karen <karen.avakov2@gmail.com>
2025-06-15 15:56:40 +0300

    добавил проверку пингов

--- routes/monitor/monitor.js
+++ routes/monitor/monitor.js
@@ -4,6 +4,7 @@ const router = Router();
 
 router.get('/', async (req, res) => {
   res.status(200).send('API is working');
+  console.log('Прием проверка связи...');
 });
 export default {
   path: '/',
