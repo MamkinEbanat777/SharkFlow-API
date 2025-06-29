837174a3a9edc9f03df56f8eb34c7b7d9273d477
Karen <karen.avakov2@gmail.com>
2025-06-29 03:31:12 +0300

    подправил корс

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,7 +1,7 @@
 export const allowedOrigins = [
   'http://localhost:5173',
   'http://localhost:4173',
-  'https://sharkflow-blyt.onrender.com',
+  'https://taskflow-blyt.onrender.com',
   'http://192.168.128.44:5173',
-  'https://sharkflow-6wu.pages.dev',
+  'https://taskflow-6wu.pages.dev',
 ];
