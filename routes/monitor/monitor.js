e6ab80184175f5fdb39a51a82b554f8ac452aeaa
Karen <karen.avakov2@gmail.com>
2025-07-03 02:24:43 +0300

    подправил цвета

--- routes/monitor/monitor.js
+++ routes/monitor/monitor.js
@@ -3,8 +3,8 @@ import { Router } from 'express';
 const router = Router();
 
 router.get('/monitor', async (req, res) => {
-  res.status(200).send('API is working');
   console.info('Прием проверка связи...');
+  res.status(200).send('API is working');
 });
 export default {
   path: '/',
