ea5eca05a3e981ab238f8cbc1697351b17d92cc8
Karen <karen.avakov2@gmail.com>
2025-07-06 20:57:28 +0300

    убрал пока менюшку

--- telegramConfig/bot.js
+++ telegramConfig/bot.js
@@ -10,12 +10,9 @@ const bot = new Telegraf(process.env.TELEGRAM_BOT_TOKEN);
 
 const commandsDir = path.resolve(__dirname, 'commands');
 const eventsDir = path.resolve(__dirname, 'events');
-const menusDir = path.resolve(__dirname, 'menus');
-const actionsDir = path.resolve(__dirname, 'actions');
 
 await loadConfig(bot, commandsDir);
 await loadConfig(bot, eventsDir);
-await loadConfig(bot, menusDir);
-await loadConfig(bot, actionsDir);
+
 
 export default bot;
--- telegramConfig/withAuth.js
+++
@@ -1,9 +0,0 @@
-import { authenticateTelegramMiddleware } from '../middlewares/http/authenticateTelegramMiddleware.js';
-
-export default function withAuth(handler) {
-  return async (ctx) => {
-    await authenticateTelegramMiddleware(ctx, async () => {
-      await handler(ctx);
-    });
-  };
-}
