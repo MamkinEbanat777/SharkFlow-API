8efb032c03113cb6a58706d5dfeef79356ac5102
Karen <karen.avakov2@gmail.com>
2025-10-15 22:18:03 +0300

    ошибку нашел, логи убрал

--- middlewares/http/validateMiddleware.js
+++ middlewares/http/validateMiddleware.js
@@ -1,13 +1,10 @@
 export const validateMiddleware = (schema) => async (req, res, next) => {
-       console.info("Payload до валидации:", req.body);
-
   try {
     const validated = await schema.validate(req.body, {
       abortEarly: false,
     });
 
     req.validatedBody = validated;
-    console.info("Пришедщие данные:",req.validatedBody)
     next();
   } catch (error) {
     let details;
@@ -19,8 +16,7 @@ export const validateMiddleware = (schema) => async (req, res, next) => {
     } else {
       details = [error.message || 'Unknown validation error'];
     }
-    
-    console.error("Ошибка валидации:", details);
+
     res.status(400).json({
       error: 'Ошибка валидации данных',
       details,
